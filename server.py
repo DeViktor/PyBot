be2d30b0b62e9f957515a54ae88b353c4b41a3f0
DeViktor <contato.4years@gmail.com>
2021-10-14 11:46:42 -0400

    REACTION EMOJIS ADDED

--- server.py
+++ server.py
@@ -19,10 +19,21 @@ async def on_message(message):
 
     await bot.process_commands(message)
 
+@bot.event
+async def on_reaction_add(reaction, user):
+    print(reaction.emoji)
+
+    if reaction.emoji == "🇦":
+        role = user.guild.get_role(889314289826037802)
+        await user.add_roles(role)
+    elif reaction.emoji == "🇧":
+        role = user.guild.get_role(898026225677774868)
+        await user.add_roles(role)
+
 @bot.command(name= "nlw")
 async def cmd(ctx):
     response = "vem ai NLW HEAT dia 17 fml vamo q vamo"
     
     await ctx.send(response)
 
-bot.run("YOUR BOT TOKEN HERE")
+bot.run("YOUR BOT TOKER HERE")
